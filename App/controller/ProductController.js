6475d519c038c6b770a9748bafe3eaa70ccfca26
kayalraju <87748199+kayalraju@users.noreply.github.com>
2024-10-18 09:52:12 +0530

    dgagdys

--- App/controller/ProductController.js
+++ App/controller/ProductController.js
@@ -1,10 +1,23 @@
 
 const Product = require('../model/product'); 
-
+const fs = require('fs');
+const path = require('path');
+const { Validator } = require('node-input-validator');
 class ProductController {
     async craetreProduct(req,res){
       //console.log(req.body);
       try{
+        const v=new Validator(req.body,{
+            name:'required|minLength:3|maxLength:20',
+            price:'required',
+            size:'required'
+        });
+        const matched=await v.check().then((val)=>val);
+        if(!matched){
+            return res.status(400).json({
+                message:v.errors
+            })
+        }
         const {name,price,size} = req.body;
         const file=req.file;
         if(!file){
@@ -62,6 +75,7 @@ class ProductController {
 
     async updateProduct(req,res){
       try{
+        
         const id = req.params.id;
         const {name,price,size} = req.body;
         const data = await Product.findByIdAndUpdate(id,{
@@ -81,11 +95,18 @@ class ProductController {
     async deleteProduct(req,res){
       try{
         const id = req.params.id;
-        const data = await Product.findByIdAndDelete(id);
-        return res.status(200).json({
-          status:200,
-          message:"data deleted successfully",
-        })
+        const dlee = await Product.findByIdAndDelete(id);
+        //console.log('kk',dlee.image);
+        const filePath = path.join(__dirname, 'uploads');
+        console.log('aaa',filePath);
+        
+           fs.unlinkSync(filePath.dlee.image);
+          return res.status(200).json({
+            status:200,
+            message:"data deleted successfully",
+          })
+         
+       
       }catch(err){
         console.log(err);
       }
