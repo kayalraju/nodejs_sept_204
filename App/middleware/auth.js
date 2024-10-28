f2edccfb6ba6101933b5ab90bb1e71c3151519c8
kayalraju <87748199+kayalraju@users.noreply.github.com>
2024-10-28 11:30:06 +0530

    auth middleware next

--- App/middleware/auth.js
+++ App/middleware/auth.js
@@ -1,5 +1,5 @@
 const  bcrypt=require('bcryptjs');
-
+const jwt=require('jsonwebtoken');
 const hashpassword=async(password)=>{
     try{
         const salt=await bcrypt.genSalt(10);
@@ -16,4 +16,32 @@ const comparepassword=async(password,hashedPassword)=>{
 }
 
 
-module.exports={hashpassword,comparepassword}
+
+// Define AuthCheck middleware (function). When a user successfully login, he/she will be under protected route. So, this funciton (middleware) make a route protected. This function is used to check if a valid JSON Web Token (JWT) is provided with a request or not
+const authCheck = async (req, res, next) => {
+    // 1. Extract the Token from the incoming Request
+    const token =
+      req.body.token || req.query.token || req.headers["x-access-token"];
+  
+    // 2. Check if the Token is Present or not in incoming request. If the token not present, do the following
+    if (!token) {
+      return res.status(403).json({
+        message: "A token is required for authentication",
+      });
+    }
+  
+    // 3. If the token present in incoming requrest, Verify the Token
+    try {
+      // 1. Verify the token and attach the decoded user to the request object
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+  
+      // 2. Control passes to the next middleware or route handler
+      next();
+    } catch (err) {
+      console.log(err);
+      return res.status(401).json({ message: "invalid Token Access" });
+    }
+  };
+
+module.exports={hashpassword,comparepassword,authCheck}
--- App/router/userRouter.js
+++ App/router/userRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const userController = require('../controller/userController');
+const {authCheck} = require('../middleware/auth');
 const router = express.Router();
 
 router.post('/register',userController.register );
 router.post('/login',userController.login );
-router.get('/user/dashboard',userController.dashboard );
+router.get('/user/dashboard',authCheck,userController.dashboard );
 
 
 
